33018a489e00292085588d13173d48ebca85f249
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-11-01 12:26:09 +0100

    Update 1507. Reformat Date.py

--- Easy/1507. Reformat Date.py
+++ Easy/1507. Reformat Date.py
@@ -4,7 +4,7 @@ class Solution:
         month = ""
         day = ""
 
-        months = {"Jan" : "1", "Feb" : "2", "Mar" : "3", "Apr" : "4", "May": "5", "Jun" : "6", "Jul" : "7", "Aug" : "8", "Sep" : "9", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
+        months = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May": "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
 
         n = len(date)
         i = 0
@@ -34,9 +34,6 @@ class Solution:
             day = "0" + day
         
         month = months[month]
-        
-        if len(month) < 2:
-            month = "0" + month
-        
+               
         return year + "-" + month + "-" + day
         
