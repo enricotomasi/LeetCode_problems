d0f60588fbe7ac431b6ec470dd66e7ded9b22479
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2024-11-27 10:09:02 +0100

    Delete 1731. The Number of Employees Which Report to Each Employee.sql

--- 1731. The Number of Employees Which Report to Each Employee.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT Employees.employee_id, Employees.name, COUNT(Employees.employee_id) AS reports_count, ROUND(avg(emp.age)) AS average_age
-FROM Employees
-JOIN Employees emp ON Employees.employee_id = emp.reports_to 
-GROUP BY Employees.employee_id, Employees.name
-ORDER BY Employees.employee_id
