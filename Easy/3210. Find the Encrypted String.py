4ad6d432d25302044f8a7535138c00b6e48113ee
Enrico Tomasi <62146815+enricotomasi@users.noreply.github.com>
2025-03-14 18:14:03 +0100

    Create 3210. Find the Encrypted String.py

---
+++ Easy/3210. Find the Encrypted String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        n = len(s)
+        ans = ""
+
+        for i in range(n):
+            ans += s[(i + k) % n]
+        
+        return ans
+        
